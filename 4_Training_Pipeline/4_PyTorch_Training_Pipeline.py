653c7b5ed00897136684c7fe76620f950bc303d2
Ravi Der <derravi00@gmail.com>
2025-11-24 23:23:38 +0530

    Updated Code

--- 4_Training_Pipeline/4_PyTorch_Training_Pipeline.py
+++ 4_Training_Pipeline/4_PyTorch_Training_Pipeline.py
@@ -9,16 +9,16 @@ print(df.head())
 
 print(f"The total number of Columns is {df.shape[1]} and the total number of Rows is {df.shape[0]}.")
 
-df.drop(columns=['id'],inplace=True)
+df.drop(columns=['id'], inplace=True)
 
 print(f"The total number of Columns is {df.shape[1]} and the total number of Rows is {df.shape[0]}.")
 
 df['diagnosis'].unique()
 
-x = df.iloc[:,1:] #All Colum Excepted 'diagnosis'
-y = df.iloc[:,0] # 'diagnosis' Column
+x = df.iloc[:, 1:]  # All columns except 'diagnosis'
+y = df.iloc[:, 0]   # 'diagnosis' column
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
@@ -28,7 +28,7 @@ lb = LabelEncoder()
 y_train = lb.fit_transform(y_train)
 y_test = lb.transform(y_test)
 
-#Conver the all numpy Arrays to the Tensors.
+# Convert numpy arrays to tensors
 x_train_tensor = torch.from_numpy(x_train)
 x_test_tensor = torch.from_numpy(x_test)
 y_train_tensor = torch.from_numpy(y_train)
@@ -36,65 +36,67 @@ y_test_tensor = torch.from_numpy(y_test)
 
 print(x_test_tensor.shape)
 
+
 class MyfirstNN():
-    def __init__(self,x):
-        self.weights = torch.rand(x.shape[1],dtype=torch.float64,requires_grad=True)
-        self.bias = torch.zeros(1,dtype=torch.float64,requires_grad=True)
+    def __init__(self, x):
+        self.weights = torch.rand(x.shape[1], dtype=torch.float64, requires_grad=True)
+        self.bias = torch.zeros(1, dtype=torch.float64, requires_grad=True)
 
-    def forward(self,x):
-        z = torch.matmul(x,self.weights) + self.bias
-        y_predict = torch.sigmoid(z)
-        return y_predict
+    def forward(self, x):
+        z = torch.matmul(x, self.weights) + self.bias
+        y_predict = torch.sigmoid(z)
+        return y_predict
 
-    def loss_function(self,y_pred,y):
-        #Clamp Predictions to avoid log(0)
-        epsilon = 1e-7
-        y_pred = torch.clamp(y_pred,epsilon,1-epsilon)
+    def loss_function(self, y_pred, y):
+        # Clamp Predictions to avoid log(0)
+        epsilon = 1e-7
+        y_pred = torch.clamp(y_pred, epsilon, 1 - epsilon)
 
-        #calculate loss
-        loss = -(y_train_tensor*torch.log(y_pred) + (1 - y_train_tensor) * torch.log(1 - y_pred)).mean()
-        return loss
+        # Calculate loss
+        loss = -(y_train_tensor * torch.log(y_pred) +
+                 (1 - y_train_tensor) * torch.log(1 - y_pred)).mean()
+        return loss
+
 
 learning_rate = 0.1
 epochs = 20
 
-#Create Model
+# Create model
 model = MyfirstNN(x_train_tensor)
 
-#Define Loop
+# Training loop
 for i in range(epochs):
- 
-    #Forward Pass
-    y_predict = model.forward(x_train_tensor)
- 
-    #Loss Calculation
-    loss = model.loss_function(y_predict,y_train_tensor)
- 
-    #Backward Pass
-    loss.backward()
- 
-    #Parameters Updates.
-    with torch.no_grad():
-        model.weights -= learning_rate * model.weights.grad
-        model.bias -= learning_rate * model.bias.grad
 
-    #zero gradients
-    model.weights.grad.zero_()
-    model.bias.grad.zero_()
+    # Forward pass
+    y_predict = model.forward(x_train_tensor)
 
-    #Print Loss in each epoch
-    print(f"Epoch: {i + 1}, Loss : {loss.item()}.")
+    # Loss calculation
+    loss = model.loss_function(y_predict, y_train_tensor)
 
-print(f"Model Weights:{model.weights}")
+    # Backward pass
+    loss.backward()
 
-print(f"Model Bias:{model.bias}")
+    # Update parameters
+    with torch.no_grad():
+        model.weights -= learning_rate * model.weights.grad
+        model.bias -= learning_rate * model.bias.grad
 
-#Evaluation of the Model
+    # Zero gradients
+    model.weights.grad.zero_()
+    model.bias.grad.zero_()
 
+    # Print loss per epoch
+    print(f"Epoch: {i + 1}, Loss: {loss.item()}")
+
+
+print(f"Model Weights: {model.weights}")
+print(f"Model Bias: {model.bias}")
+
+# Evaluation
 with torch.no_grad():
-    y_pred = model.forward(x_test_tensor)
-    y_pred = (y_pred>0.9).float()
+    y_pred = model.forward(x_test_tensor)
+    y_pred = (y_pred > 0.9).float()
 
-    #Accuracy score of the ML NN Pipeline
-    accuracy = (y_pred == y_test_tensor).float().mean()
-    print(f"The Accuracy is : {accuracy}")
+    # Accuracy
+    accuracy = (y_pred == y_test_tensor).float().mean()
+    print(f"The Accuracy is: {accuracy}")
--- 5_NN_Module/5_NN_Module.py
+++ 5_NN_Module/5_NN_Module.py
@@ -1,100 +1,41 @@
-import pandas as pd
-import numpy as np
-import torch
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+"""
+# NN Module :- Nural Network Module
+# torch.optim module
+-> An NN module is a small part of a neural network that takes some input, does a calculation, and gives an output. Many modules are combined together to build a full neural network.
+-> NN Module = Neural Network ka part (layer/function) jo input leta hai, kuch operation karta hai, aur output deta hai.
+Many modules → full neural network.
+-> In PyTorch, an NN module is a building block used to create neural networks. Every layer (like Linear, Conv2d, ReLU, etc.) is written as a module. You combine many modules to build a full model.
 
-df = pd.read_csv("breast-cancer.csv")
-print(df.head())
+->Ex.
+nn.Linear → linear layer module
+nn.ReLU → activation module
+nn.Conv2d → convolution module
+nn.Dropout → dropout module
+nn.Sequential → modules ko ek line me jodna
+nn.Module → base class jisse sare modules bante hain
 
-print(f"The total number of Columns is {df.shape[1]} and the total number of Rows is {df.shape[0]}.")
+#Lets see the Key Component of this NN Module.
 
-df.drop(columns=['id'],inplace=True)
+(1) Modules(Layers):-
+-> NN module = one piece of a neural network that performs a specific task, and you connect multiple pieces to create the complete model.
+-> Common Layers :- Whenever we are use some layers into multiple times into our Nural Network so this is called Common Layers that we are use a multiple times into our NN or you cany say that we are use those layers into the each nural network. So this is our common layers.
+    -> like:- nn.Linear,nn.Conv2d,nn.LSTM,
 
-print(f"The total number of Columns is {df.shape[1]} and the total number of Rows is {df.shape[0]}.")
+(2) Activation Function:-
+-> Neural network me activation function ek aisa function hota hai jo decide karta hai ki ek neuron ka output kya hoga. Yeh input me kuch non-linearity add karta hai, taaki neural network complex patterns seekh sake.
+-> Socho neural network ke neurons ko switches ki tarah, aur activation function ko ek aise controller ki tarah jo decide karta hai ki switch kab on hoga aur kab off. Yeh ensure karta hai ki network linear nahi, balki complex patterns bhi samajh paye.
 
-df['diagnosis'].unique()
+(3) Loss Function:-
+-> Its Provide loss function such as nn.CrossEntropyLoss,nn.MSELoss and nn.NLLLoss to quantify the difference between the models predictions and actual targets.
 
-x = df.iloc[:,1:] #All Colum Excepted 'diagnosis'
-y = df.iloc[:,0] # 'diagnosis' Column
+(4) Container Modules:- When we are use multiple layers for making a NN so we are able to make one container of this all layers.
+-> nn.Sequential :- A sequancial container it takes layers in order.
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
+(5) Regularization and dropout.
+-> Layers like nn.Dropout and nn.BatchNorm2d help prevent overfitting and improv the models ability to generalize to new data.
 
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.transform(x_test)
+# torch.optim
+-> torch.optim is a collection of optimizers in PyTorch that adjust the model’s weights during training so the model learns better.
 
-lb = LabelEncoder()
-y_train = lb.fit_transform(y_train)
-y_test = lb.transform(y_test)
 
-#Conver the all numpy Arrays to the Tensors.
-x_train_tensor = torch.from_numpy(x_train)
-x_test_tensor = torch.from_numpy(x_test)
-y_train_tensor = torch.from_numpy(y_train)
-y_test_tensor = torch.from_numpy(y_test)
-
-print(x_test_tensor.shape)
-
-class MyfirstNN():
-    def __init__(self,x):
-        self.weights = torch.rand(x.shape[1],dtype=torch.float64,requires_grad=True)
-        self.bias = torch.zeros(1,dtype=torch.float64,requires_grad=True)
-
-    def forward(self,x):
-        z = torch.matmul(x,self.weights) + self.bias
-        y_predict = torch.sigmoid(z)
-        return y_predict
-
-    def loss_function(self,y_pred,y):
-        #Clamp Predictions to avoid log(0)
-        epsilon = 1e-7
-        y_pred = torch.clamp(y_pred,epsilon,1-epsilon)
-
-        #calculate loss
-        loss = -(y_train_tensor*torch.log(y_pred) + (1 - y_train_tensor) * torch.log(1 - y_pred)).mean()
-        return loss
-
-learning_rate = 0.1
-epochs = 20
-
-#Create Model
-model = MyfirstNN(x_train_tensor)
-
-#Define Loop
-for i in range(epochs):
- 
-    #Forward Pass
-    y_predict = model.forward(x_train_tensor)
- 
-    #Loss Calculation
-    loss = model.loss_function(y_predict,y_train_tensor)
- 
-    #Backward Pass
-    loss.backward()
- 
-    #Parameters Updates.
-    with torch.no_grad():
-        model.weights -= learning_rate * model.weights.grad
-        model.bias -= learning_rate * model.bias.grad
-
-    #zero gradients
-    model.weights.grad.zero_()
-    model.bias.grad.zero_()
-
-    #Print Loss in each epoch
-    print(f"Epoch: {i + 1}, Loss : {loss.item()}.")
-
-print(f"Model Weights:{model.weights}")
-
-print(f"Model Bias:{model.bias}")
-
-#Evaluation of the Model
-
-with torch.no_grad():
-    y_pred = model.forward(x_test_tensor)
-    y_pred = (y_pred>0.9).float()
-
-    #Accuracy score of the ML NN Pipeline
-    accuracy = (y_pred == y_test_tensor).float().mean()
-    print(f"The Accuracy is : {accuracy}")
+"""
